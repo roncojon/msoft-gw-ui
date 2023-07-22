fed1d5f5ec93f3397b2df71737e8bd3e826e623a
RonaldGS <rguillen@datamart.cl>
2023-07-22 16:41:22 -0400

    TEST

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import './App.css';
 import SnackbarError from './components/common/SnackBarError';
 import GatewaysContainer from './components/containers/Gateways/Gateways';
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
+
 function App() {
 const {error} = useQueryErrorResetBoundary();
-console.log('error making request')
+console.log('error making request :)')
 console.log(error)
   return (
     <>
